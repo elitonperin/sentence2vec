784647c3a434f084c5cf14c67cd85554ac63dc17
Eliton Scardin Perin <elitonperin@gmail.com>
2015-10-28 19:18:03 -0300

    adaptação

--- demo.py
+++ demo.py
@@ -16,7 +16,7 @@ from word2vec import Word2Vec, Sent2Vec, LineSentence
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("running %s" % " ".join(sys.argv))
 
-input_file = 'test.txt'
+input_file = '../data/text8'
 model = Word2Vec(LineSentence(input_file), size=100, window=5, sg=0, min_count=5, workers=8)
 model.save(input_file + '.model')
 model.save_word2vec_format(input_file + '.vec')
